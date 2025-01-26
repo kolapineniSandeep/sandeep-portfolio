3cace95bb796e74ac3178f905f2092d9ceac23df
sandeep Kolapineni <sandeepkolapineni@gmail.com>
2025-01-25 22:28:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+### Setup
+
+Get the code
+
+<pre>git clone https://github.com/ubaimutl/react-portfolio.git</pre>
+ 
+Install required dependencies
+
+<pre>yarn install</pre>
+
+
+Start the server
+
+<pre>yarn start</pre>
+
+
---
+++ src/content_option.js
@@ -0,0 +1,112 @@
+const logotext = "SANDEEP KOLAPINENI";
+const meta = {
+    title: "SANDEEP KOLAPINENI",
+    description: "I’m SANDEEP KOLAPINENI, a full stack developer interested in AI and ML. I develop apps and websites.",
+};
+
+const introdata = {
+    title: "I’m SANDEEP KOLAPINENI",
+    animated: {
+        first: "I love technology",
+        second: "I am a full stack developer interested in AI and ML",
+        third: "I develop apps and websites",
+    },
+    description: "As a seasoned Software Engineer with over 7 years of hands-on experience, I have honed my skills in developing robust software applications.",
+    your_img_url: "../../assets/images/img1.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am a seasoned Software Engineer with over 7 years of hands-on experience, I have honed my skills in developing robust software applications. My proficiency spans across various IT technologies, reflecting a deep-rooted passion for tackling complex challenges and driving innovation. I thrive on opportunities that push boundaries, allowing me to blend personal development with professional growth. Committed to excellence, I continuously strive to remain at the forefront of the dynamic landscape of software engineering, delivering outstanding results with every project I undertake.",
+};
+
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "flask",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name:"Java",
+        value: 90,
+    },
+    {
+        name: "Spring Boot",
+        value: 85,
+    }
+];
+
+const services = [
+    {
+    title: "Backend REST API Development",
+    description: "Expertise in developing robust and scalable REST APIs using modern backend technologies.",
+    },
+    {
+    title: "AI Integrations",
+    description: "Integrating AI solutions to enhance application capabilities and user experiences.",
+    },
+    {
+    title: "ML Model Training",
+    description: "Training machine learning models to solve complex problems and drive innovation.",
+    },
+
+];
+
+const dataportfolio = [{
+    img: "https://github.com/kolapineniSandeep/AI-healthcare-chatbot/blob/main/demo1.png?raw=true",
+  
+    description: "AI chatbot for healthcare is a powerful tool that can assist patients with booking and managing appointments, detecting diseases based on symptoms.",
+    link: "https://github.com/kolapineniSandeep/AI-healthcare-chatbot",
+    },
+    {
+    img: "https://user-images.githubusercontent.com/98118151/233203551-7111712a-3fea-416c-811f-08db161a546f.png",
+
+    description: "CookingBot is a chatbot trained using a Bidirectional LSTM neural network to assist users with their cooking-related questions and needs.",
+    link: "https://github.com/kolapineniSandeep/cookingbot"
+    },
+    {
+    img: "https://github-readme-stats.vercel.app/api?username=kolapinenisandeep&theme=dark&show_icons=true",
+
+    description: "find my github projects here",
+    link: "https://github.com/kolapineniSandeep/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sandeepkolapineni@gmail.com",
+    YOUR_FONE: "(XXX)-XXX-XXXX",
+    description: "I am available for freelance work. Connect with me via email",
+    
+    YOUR_SERVICE_ID: "service_stpbklp",
+    YOUR_TEMPLATE_ID: "template_9o06s9f",
+    YOUR_USER_ID: "4J2sDDvMYRUlfH9pU",
+};
+
+const socialprofils = {
+    github: "https://github.com/kolapineniSandeep/",
+    facebook: "https://facebook.com/",
+    linkedin: "https://www.linkedin.com/in/kolapinenisandeep/",
+    twitter: "https://twitter.com/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
